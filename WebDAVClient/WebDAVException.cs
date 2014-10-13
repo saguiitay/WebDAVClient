c6437f7ccae087b4cca2b1ee304ec54bcfb6a3ab
Itay Sagui <saguiitay@hotmail.com>
2014-10-13 23:11:01 +0300

    Updated assembly version to 1.0.3 Added missing WebDAVException file

---
+++ WebDAVClient/WebDAVException.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WebDAVClient
+{
+    public class WebDAVException : HttpException
+    {
+        public WebDAVException()
+        {
+        }
+
+        public WebDAVException(string message) 
+            : base(message)
+        {}
+
+        public WebDAVException(string message, int hr) 
+            : base(message, hr)
+        {}
+
+        public WebDAVException(string message, Exception innerException) 
+            : base(message, innerException)
+        {}
+
+        public WebDAVException(int httpCode, string message, Exception innerException) 
+            : base(httpCode, message, innerException)
+        {}
+
+        public WebDAVException(int httpCode, string message) 
+            : base(httpCode, message)
+        {}
+
+        public WebDAVException(int httpCode, string message, int hr) 
+            : base(httpCode, message, hr)
+        {}
+
+        protected WebDAVException(SerializationInfo info, StreamingContext context) 
+            : base(info, context)
+        {}
+    }
+}
