652d09f380becd93f82a4276a80a18df69fa38ec
Itay Sagui <saguiitay@hotmail.com>
2015-03-13 14:37:38 +0200

    Updated TestWebDAVClient to reflect latest changes, and include remarks

--- TestWebDAVClient/Program.cs
+++ TestWebDAVClient/Program.cs
@@ -1,32 +1,54 @@
 ﻿using System.IO;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 
 namespace TestWebDAVClient
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+
+        private static async Task MainAsync()
         {
             // Basic authentication required
             var c = new WebDAVClient.Client(new NetworkCredential { UserName = "USERNAME" , Password = "PASSWORD"});
             c.Server = "https://webdav.4shared.com";
             c.BasePath = "/";
 
-            var files = c.List("/").Result;
+            // List items in the root folder
+            var files = await c.List();
+            // Find first folder in the root folder
             var folder = files.FirstOrDefault(f => f.Href.EndsWith("/Test/"));
-            var folderReloaded = c.Get(folder.Href).Result;
+            // Load a specific folder
+            var folderReloaded = await c.GetFolder(folder.Href);
 
-            var folderFiles = c.List(folder.Href).Result;
+            // List items in the folder
+            var folderFiles = await c.List(folderReloaded.Href);
+            // Find a file in the folder
             var folderFile = folderFiles.FirstOrDefault(f => f.IsCollection == false);
 
-            var x = c.Download(folderFile.Href).Result;
+            var tempFileName = Path.GetTempFileName();
 
+            // Download item into a temporary file
+            using (var tempFile = File.OpenWrite(tempFileName))
+            using (var stream = await c.Download(folderFile.Href))
+                await stream.CopyToAsync(tempFile);
+
+            // Update file back to webdav
             var tempName = Path.GetRandomFileName();
-            var fileUploaded = c.Upload(folder.Href, File.OpenRead(@"C:\Users\itay.TZUNAMI\Desktop\Untitled.png"), tempName).Result;
+            using (var fileStream = File.OpenRead(tempFileName))
+            {
+                var fileUploaded = await c.Upload(folder.Href, fileStream, tempName);
+            }
             
+            // Create a folder
             tempName = Path.GetRandomFileName();
-            var folderCreated = c.CreateDir(folder.Href, tempName).Result;
+            var folderCreated = await c.CreateDir(folder.Href, tempName);
 
         }
     }
